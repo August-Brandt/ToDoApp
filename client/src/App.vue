4a6ccdf3f8f640ca92a9add9e3b6e6bb576217bf
August-Brandt <augustbrandt170@gmail.com>
2025-05-28 16:09:57 +0200

    Add navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import Navbar from '@/components/Navbar.vue'
 
 const name = "August";
 const clicks = ref(0);
@@ -24,6 +25,7 @@ const removeTodo = (index) => {
 </script>
 
 <template>
+  <Navbar/>
   <h1>Vue ToDo</h1>
   <h3>Hello {{ name }}</h3>
   <button @click="clicking">Clicked {{ clicks }} times</button>
@@ -32,9 +34,12 @@ const removeTodo = (index) => {
     <input type="text" id="newTodo" name="newTodo" v-model="newTodo">
     <button type="submit">Add Todo</button>
   </form>
-  <ul class="ml-3">
+  <h3 v-if="tasks.length > 0" class="ml-3 underline text-xl">ToDos:</h3>
+  <ul class="ml-5">
     <li v-for="(task, index) in tasks" :key="task"> 
-      <button @click="removeTodo(index)" class="mr-2 bg-gray-300 center text-red-600 border-2 rounded-sm text-center align-middle w-5 h-5 font-bold">x</button>
+      <button @click="removeTodo(index)" class="mr-2 bg-gray-300 center text-red-600 border-2 rounded-sm text-center w-5 h-5 font-bold flex justify-center">
+        <i class="pi pi-times align-middle"></i>
+      </button>
       <span class="font-bold underline">
         {{ task }}
       </span>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import logo from '@/assets/img/logo.png'
+
+</script>
+
+<template>
+  <nav class="bg-green-400 border-b-2 border-green-100">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-15 items-center justify-between">
+        <div class="flex flex-1 items-center justify-center md:justify-start">
+          <!-- Logo -->
+          <a href="/" class="flex flex-shrink-0 items-center mr-4">
+            <img :src="logo" alt="logo" class="h-10 w-auto bg-white rounded-2xl">
+            <span class="hidden md:block text-white text-xl font-extrabold ml-2">
+              Vue ToDo
+            </span>
+          </a>
+          
+          <div class="">
+            <div class="flex space-x-2">
+              <a href="/" class="text-white text-l font-bold bg-green-600 py-1 px-2 rounded-lg hover:bg-green-800">
+                Home
+              </a>
+  
+              <a href="/todos" class="text-white text-l font-bold py-1 px-2 rounded-lg hover:bg-green-800">
+                ToDo List
+              </a>
+  
+              <a href="/about" class="text-white text-l font-bold  py-1 px-2 rounded-lg hover:bg-green-800">
+                About
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav> 
+</template>
