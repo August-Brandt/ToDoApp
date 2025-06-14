fdfbf47a54201fd37f06bce9186c16ca22d4b37f
August-Brandt <augustbrandt170@gmail.com>
2025-06-14 14:41:02 +0200

    Create todos from json file

--- client/src/components/ToDoCards.vue
+++ client/src/components/ToDoCards.vue
@@ -1,34 +1,18 @@
 <script setup>
 import Card from "@/components/Card.vue"
+import todoData from "@/todos.json"
+import { ref } from 'vue'
 
+const todos = ref(todoData);
+console.log(todos.value);
 </script>
 
 <template>
 <section class="p-6">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
-        <Card>
-            <h2 class="text-lg font-bold">Todo Title 1</h2>
-            <p>
-                Here is a discription of what the todo is about.
-            </p>
-        </Card>
-        <Card>
-            <h2 class="text-lg font-bold">Todo Title 2</h2>
-            <p>
-                Here is a discription of what the todo is about.
-            </p>
-        </Card>
-        <Card>
-            <h2 class="text-lg font-bold">Todo Title 3</h2>
-            <p>
-                Here is a discription of what the todo is about.
-            </p>
-        </Card>
-        <Card>
-            <h2 class="text-lg font-bold">Todo Title 4</h2>
-            <p>
-                Here is a discription of what the todo is about.
-            </p>
+        <Card v-for="todo in todos" :key="todo.id">
+            <h1 class="text-lg font-bold">{{ todo.title }}</h1>
+            <p>{{ todo.description }}</p>
         </Card>
     </div>
 </section>
