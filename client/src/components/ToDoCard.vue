6e821733f11dfc7e03e4cddcd48da764cf0d6060
August-Brandt <augustbrandt170@gmail.com>
2025-06-14 15:05:35 +0200

    Add todo card component

---
+++ client/src/components/ToDoCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from "@/components/Card.vue";
+import { defineProps } from "vue";
+
+defineProps({
+    todo: Object,
+});
+</script>
+
+<template>
+    <Card>
+        <h1 class="text-lg font-bold">{{ todo.title }}</h1>
+        <p>
+            {{ todo.description }}
+        </p>
+        <div class="border border-gray-300 mt-2"></div>
+        <h3 class="text-sm text-gray-500">Finish by: {{ todo.doDate }}</h3>
+    </Card>
+</template>
--- client/src/components/ToDoCards.vue
+++ client/src/components/ToDoCards.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Card from "@/components/Card.vue"
-import todoData from "@/todos.json"
-import { ref } from 'vue'
+import ToDoCard from "./ToDoCard.vue";
+import todoData from "@/todos.json";
+import { ref } from 'vue';
 
 const todos = ref(todoData);
 console.log(todos.value);
@@ -10,10 +10,7 @@ console.log(todos.value);
 <template>
 <section class="p-6">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
-        <Card v-for="todo in todos" :key="todo.id">
-            <h1 class="text-lg font-bold">{{ todo.title }}</h1>
-            <p>{{ todo.description }}</p>
-        </Card>
+        <ToDoCard v-for="todo in todos" :key="todo.id" :todo="todo"/>
     </div>
 </section>
 
