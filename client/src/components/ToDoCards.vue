48edbfc189c0cda574d034d7a27509691e0150af
August-Brandt <augustbrandt170@gmail.com>
2025-06-15 16:55:32 +0200

    Move done todos to back of list

--- client/src/components/ToDoCards.vue
+++ client/src/components/ToDoCards.vue
@@ -1,20 +1,34 @@
 <script setup>
 import ToDoCard from "./ToDoCard.vue";
 import todoData from "@/todos.json";
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const todos = ref(todoData);
 
 const removeTodo = (index) => {
     todos.value.splice(index, 1);
 }
+ 
+const sortedTodos = computed(() => {
+    todos.value.sort(( a, b ) => {
+        if (a.finished && b.finished) {
+            return 0;
+        }
+        if (a.finished && !b.finished) {
+            return 1;
+        }
+        return -1;
+    });
+    console.log(todos);
+    return todos.value;
+});
 
 </script>
 
 <template>
 <section class="p-6">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
-        <ToDoCard v-for="(todo, index) in todos" :key="todo.id" :todo="todo" :removeCallBack="(index) => removeTodo(index)"/>
+        <ToDoCard v-for="(todo, index) in sortedTodos" :key="todo.id" :todo="todo" :removeCallBack="(index) => removeTodo(index)"/>
     </div>
 </section>
 
