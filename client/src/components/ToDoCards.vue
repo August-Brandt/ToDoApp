1091a3e86bd9608118595aaf8ac6448f9e28d723
August-Brandt <augustbrandt170@gmail.com>
2025-06-15 17:16:01 +0200

    Fix issue with removing todos not working correctly

--- client/src/components/ToDoCards.vue
+++ client/src/components/ToDoCards.vue
@@ -5,8 +5,10 @@ import { ref, computed } from 'vue';
 
 const todos = ref(todoData);
 
-const removeTodo = (index) => {
-    todos.value.splice(index, 1);
+const removeTodo = (id) => {
+    todos.value = todos.value.filter((todo) => {
+        return todo.id != id;
+    });
 }
  
 const sortedTodos = computed(() => {
@@ -28,7 +30,7 @@ const sortedTodos = computed(() => {
 <template>
 <section class="p-6">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
-        <ToDoCard v-for="(todo, index) in sortedTodos" :key="todo.id" :todo="todo" :removeCallBack="(index) => removeTodo(index)"/>
+        <ToDoCard v-for="todo in sortedTodos" :key="todo.id" :todo="todo" :removeCallBack="() => removeTodo(todo.id)"/>
     </div>
 </section>
 
