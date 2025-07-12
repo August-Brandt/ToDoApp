8fc92145d23469670abbeef7f9be6d2aa1173bd9
August-Brandt <augustbrandt170@gmail.com>
2025-07-12 16:22:55 +0200

    Database table initialization and inserting function

--- server/src/go.mod
+++ server/src/go.mod
@@ -1,3 +1,5 @@
 module ToDoServer
 
 go 1.22.5
+
+require github.com/mattn/go-sqlite3 v1.14.28
--- server/src/main.go
+++ server/src/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Todo struct {
+	Id          int
+	Title       string
+	Description string
+	Dodate      string
+	Finished    bool
+}
+
 func main() {
+	// Setup sqlite database
+	db, err := sql.Open("sqlite3", "../database/ToDoDatabase.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Init tables if not exists
+	tableStmt := `
+	CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		description TEXT NOT NULL,
+		dodate TEXT,
+		finished INTEGER NOT NULL
+	);
+	`
+
+	_, err = db.Exec(tableStmt)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	// Run web server
 	fs := http.FileServer(http.Dir("../../client/dist"))
 	http.Handle("/", fs)
 	port, exists := os.LookupEnv("PORT")
@@ -20,4 +56,17 @@ func main() {
 
 	log.Printf("Server running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func AddTodo(db *sql.DB, todo *Todo) error {
+	stmt := `
+	INSERT INTO todos (title, description, dodate, finished)
+	VALUES (?, ?, ?, 0);
+	`
+	_, err := db.Exec(stmt, todo.Title, todo.Description, todo.Dodate)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
