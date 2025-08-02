75fa3269e5d863c8ea426a0fb057dbcfcf729f3c
August Brandt <augustbrandt170@gmail.com>
2025-08-02 20:24:45 +0200

    Update test data script to string id

--- server/database/insertTestData.sql
+++ server/database/insertTestData.sql
@@ -1,23 +1,23 @@
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 1", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("1", "Todo 1", "Description of this todo", "2025-08-25", 0);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 2", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("2", "Todo 2", "Description of this todo", "2025-08-25", 0);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 3", "Description of this todo", "2025-08-25", 1);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("3", "Todo 3", "Description of this todo", "2025-08-25", 1);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 4", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("4", "Todo 4", "Description of this todo", "2025-08-25", 0);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 5", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("5", "Todo 5", "Description of this todo", "2025-08-25", 0);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 6", "Description of this todo", "2025-08-25", 1);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("6", "Todo 6", "Description of this todo", "2025-08-25", 1);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 7", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("7", "Todo 7", "Description of this todo", "2025-08-25", 0);
 
-INSERT INTO todos (title, description, dodate, finished)
-VALUES ("Todo 8", "Description of this todo", "2025-08-25", 0);
+INSERT INTO todos (id, title, description, dodate, finished)
+VALUES ("8", "Todo 8", "Description of this todo", "2025-08-25", 0);
