87cc2f7a2b8d7db9e817c8e8cfa0144ee5e64bd1
August Brandt <augustbrandt170@gmail.com>
2025-08-02 13:32:53 +0200

    Add sql script to add test data to database

---
+++ server/database/insertTestData.sql
@@ -0,0 +1,23 @@
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 1", "Description of this todo", "2025-08-25", 0);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 2", "Description of this todo", "2025-08-25", 0);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 3", "Description of this todo", "2025-08-25", 1);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 4", "Description of this todo", "2025-08-25", 0);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 5", "Description of this todo", "2025-08-25", 0);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 6", "Description of this todo", "2025-08-25", 1);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 7", "Description of this todo", "2025-08-25", 0);
+
+INSERT INTO todos (title, description, dodate, finished)
+VALUES ("Todo 8", "Description of this todo", "2025-08-25", 0);
